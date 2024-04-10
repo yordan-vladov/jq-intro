a9bb03c2aaa8abea1352f5f6d6c962af85726220
Blago29 <126661642+Blago29@users.noreply.github.com>
2024-04-10 11:36:01 +0300

    first commit

--- task_queries.sh
+++ task_queries.sh
@@ -1,17 +1,26 @@
 # намерете всичките потребителски имена
+jq '.[].username' users.json
 
 # намерете имената на всичките потребители, които имат роля админ
+jq '.[] | select(.role == "admin") | .username' users.json
 
 # намерете броя потребители, които имат повече от една роля
+jq '[.[] | select(.roles | length > 1)] | length' users.json
 
 # намерете имената на потребителите, родени през 90-те
+jq '.[] | select(.birthdate | test("^[0-9]{4}-9[0-9]") ) | .username' users.json
 
 # намерете имената на потребителите, които живеят в северното полукълбо
+jq '.[] | select(.location.latitude > 0) | .username' users.json
 
 # намерете имената на потребителите, които работят в 'Arctiq'
+jq '.[] | select(.company == "Arctiq") | .username' users.json
 
 # намерете броя потребители, които са родени преди 1990
+jq '[.[] | select(.birthdate | startswith("199"))) ] | length' users.json
 
 # намерете името на най-старият потребител
+jq 'max_by(.birthdate) | .username' users.json
 
 # намерете имената на потребителите с празно 'about' поле
+jq '.[] | select(.about == "") | .username' users.json
